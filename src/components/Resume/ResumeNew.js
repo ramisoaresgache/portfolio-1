e49d6e4c0f114da7c91674e017787b5d9e71910c
Ramiro Soares gache <86982224+ramisoaresgache@users.noreply.github.com>
2023-02-14 21:49:55 -0300

    para ir a vercel

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/ramisoaresgache/portfolio/master/src/Assets/CV.pdf";
+  "https://raw.githubusercontent.com/ramisoaresgache/portfolio-1/master/src/Assets/CV.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
