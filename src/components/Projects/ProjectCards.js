896c5771bb94795dce52634b2a0fad5fb6b9f651
Ramiro Soares gache <86982224+ramisoaresgache@users.noreply.github.com>
2026-07-26 00:07:42 -0300

    Align project cards

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -4,44 +4,60 @@ import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 import { SiPowerbi } from "react-icons/si";
+import "./ProjectCards.css";
 
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt={`Vista previa de ${props.title}`} />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
+      <Card.Img
+        variant="top"
+        src={props.imgPath}
+        alt={`Vista previa de ${props.title}`}
+      />
+      <Card.Body className="project-card-body">
+        <Card.Title className="project-card-title">{props.title}</Card.Title>
+        <Card.Text className="project-card-description">
           {props.description}
         </Card.Text>
-        {props.powerbiLink ? (
-          <Button variant="primary" href={props.powerbiLink} target="_blank" rel="noreferrer">
-            <SiPowerbi /> &nbsp; Power BI
-          </Button>
-        ) : (
-          props.ghLink && (
-            <Button variant="primary" href={props.ghLink} target="_blank" rel="noreferrer">
-              <BsGithub /> &nbsp;{props.isBlog ? "Blog" : "GitHub"}
-            </Button>
-          )
-        )}
-        {"\n"}
-        {"\n"}
 
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            rel="noreferrer"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
-          </Button>
-        )}
+        <div className="project-card-actions">
+          {props.powerbiLink ? (
+            <Button
+              variant="primary"
+              href={props.powerbiLink}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <SiPowerbi /> &nbsp; Power BI
+            </Button>
+          ) : (
+            props.ghLink && (
+              <Button
+                variant="primary"
+                href={props.ghLink}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BsGithub /> &nbsp;{props.isBlog ? "Blog" : "GitHub"}
+              </Button>
+            )
+          )}
+
+          {!props.isBlog && props.demoLink && (
+            <Button
+              variant="primary"
+              href={props.demoLink}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <CgWebsite /> &nbsp; Demo
+            </Button>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
 }
+
 export default ProjectCards;
+
