160d9352e52e92f6511ce86033c57a1616398771
Ramiro Soares Gache <ramiro.godino@vicentelopez.gov.ar.com>
2026-07-27 10:10:11 -0300

    agregamos skills para el cv y arreglado

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -8,7 +8,7 @@ function Type() {
         strings: [
           "Data Engineer",
           "SQL - Python - ETL - Pipelines - BI",
-          "Databricks - Airflow - dbt",
+          "Spark - Hadoop - PySpark",
           "Soluciones internas y automatización de datos",
           "Estudiante de Ciencia de Datos e IA",
         ],
